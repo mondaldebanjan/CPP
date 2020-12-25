0c82b4d9fcb7bf1df89fd33b1c8d94a9b06a2d9c
mondaldebanjan <mondal.debanjan.bwn@gmail.com>
2020-12-25 13:51:37 +0000

    lvalue rvalue

--- Pointers_and_References/lval_rval.cpp
+++ Pointers_and_References/lval_rval.cpp
@@ -13,9 +13,23 @@ int& GetValue(){
     return a;
 }
 
+void SetValue(int& x){}
+void SetValue1(const int& x){}
+
 int main(){
     int a = GetValue();
     //GetValue1()=9;  <<---- This is wrong as GetValue is returning just a value whih is not a lvalue expr
     int c = GetValue()=9;  // Can use GetVal function as lval as it is having reference as return value
     cout <<c<<endl;
+
+    int x = 10;
+    SetValue(x);
+    //SetValue(10); <<--- This is wrong as argument has rvalue expr but sending a lvalue
+    SetValue1(x); // This works as below 
+    /*
+    int& y = 10; << Cannot write this but can write below
+    const int& y = 10; Why? because compiler breaks down the statement as below
+    int temp = 10; 
+    const int& y = temp;
+    */
 }
